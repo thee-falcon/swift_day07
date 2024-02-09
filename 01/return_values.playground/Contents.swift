ac64edb403b138acfa2c3c499338f92d99dfb623
thee-falcon <omar.makran.10@gmail.com>
2024-02-09 03:03:13 +0100

    some ways to code a short code in a function and use ternary to expression

--- 01/return_values.playground/Contents.swift
+++ 01/return_values.playground/Contents.swift
@@ -6,6 +6,7 @@ func    isTheSame(s1:String, s2:String)->Bool {
     return orderS1 == orderS2
     // we can use just this line of code it's the same:
     // orderS1 == orderS2
+    // we call that an expression
 }
 
 let BoolOne: Bool = isTheSame(s1: "Omar", s2: "Omar")
@@ -13,8 +14,17 @@ let BoolOne: Bool = isTheSame(s1: "Omar", s2: "Omar")
 print(BoolOne)
 
 func    pythagoras(a:Double, b:Double)->Double{
+    // we call that an expression.
     sqrt((a * a) + (b * b))
 }
 
 let c = pythagoras(a: 4, b: 5)
 print(c)
+
+// expression doesn't working in if else conditions
+// another example using ternary:
+func    greet(name:String)->String{
+    name == "Omar" ? "Oh Yes!" : "Hello, \(name)."
+}
+
+print(greet(name: "Omar"))
